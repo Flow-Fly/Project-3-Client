b96964824532955e680ede79b33969e5d54e9bdf
Flow-Fly <florian.aube@gmail.com>
2021-08-19 10:25:04 +0200

    Initial commit

---
+++ src/components/Forms/FormSignin.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { withRouter, Redirect } from "react-router-dom";
+import apiHandler from "../../api/apiHandler";
+import { withUser } from "../Auth/withUser";
+
+class FormSignin extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  handleChange = (event) => {
+    const key = event.target.name;
+    const value = event.target.value;
+
+    this.setState({ [key]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    apiHandler
+      .signin(this.state)
+      .then((data) => {
+        this.props.context.setUser(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        // Display error message here, if you set the state
+      });
+  };
+
+  render() {
+    if (this.props.context.user) {
+      return <Redirect to="/" />;
+    }
+
+    return (
+      <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+        <h2>Signin</h2>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default withRouter(withUser(FormSignin));
