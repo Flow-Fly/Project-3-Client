ae291cf53cdf87ebd46f157a0600ced3001989d5
Quentin <qmarignier@gmail.com>
2021-08-20 15:10:06 +0200

    socket.io-client

--- src/pages/Messenger/Message/Message.jsx
+++ src/pages/Messenger/Message/Message.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { format } from 'timeago.js';
 import { withUser } from '../../../components/Auth/withUser'
+import Avatar from '../../../components/Base/Avatar/Avatar';
+import './Message.css'
 
 function Message(props) {
 
     const isMine = () => {
-        return props.message.sender === props.context.user._id ? 'message mine' : 'message'
+        const sender = props.message.sender._id || props.message.sender
+        return sender === props.context.user._id ? 'mine' : ''
     }
+    
 
     return (
-        <div className={isMine()}>
-            {props.message.content}
-            {format(props.message.createdAt)}
+        <div className={`messageComponent ${isMine()}`}>
+            
+            <div className={`messageContentWrapper ${isMine()}`}>
+                <Avatar size='tiny' url={props.message.sender.profileImg || props.context.user.profileImg}/>
+                <div className={`message ${isMine()}`}>
+                    <div className={`messageContent ${isMine()}`}>
+                        {props.message.content}
+                    </div>
+                    <div className="messageTimer">
+                        {format(props.message.createdAt)}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
