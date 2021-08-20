d2b895f1ba747a5a12fb5c69a14046b4cda11b1c
Chenjia Li <cligrenet@gmail.com>
2021-08-20 18:22:23 +0200

    added job tab style

--- src/components/Job/FeedJobCard.jsx
+++ src/components/Job/FeedJobCard.jsx
@@ -1,48 +1,37 @@
 import React from 'react';
 // import Button from '../Base/Button/Button';
+import './FeedJobCard.css';
 
 const FeedJobCard = (props) => {
   const job = props.job;
   // console.log('job', job);
 
-  // const {
-  //   // _id,
-  //   title,
-  //   company,
-  //   location,
-  //   contractType,
   //   // handleDelete,
   //   // handleEdit,
-  //   // type,
+
   //   // description,
   //   // technologies,
   //   // remote,
   //   // creator,
   //   // link,
-  //   // level,
-  // } = props;
 
   return (
     <div>
-      <div>
+      <div className="FeedJobCard">
         <h5>{job.title}</h5>
-        <ul>
-          <li>{job.company}</li>
+        <ul key={job._id}>
           <li>
-            {job.location} | {job.contractType}
+            <b>{job.company}</b>
+          </li>
+          <li>
+            {job.location} |{' '}
+            {job.contractType !== undefined ? job.contractType : '...'} |
+            {job.level}
           </li>
         </ul>
 
         {/* <Button handleClick={(event) => handleEdit(_id)}>Edit</Button>
         <Button handleClick={(event) => handleDelete(_id)}>Delete</Button> */}
-
-        {/* <p>{description}</p> 
-        <p>{technologies}</p>
-        <p>{remote}</p>
-        <p>{creator}</p>
-        <p>{link}</p>
-        <p>{level}</p>
-        <p>{type}</p>*/}
       </div>
     </div>
   );
