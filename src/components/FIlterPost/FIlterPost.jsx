ead68f59a75b164a4b4d7d99d4f88fa712d3ac93
Anstetten <h.zsola@gmail.com>
2021-08-24 16:26:04 +0200

    filtering tags

--- src/components/FIlterPost/FIlterPost.jsx
+++ src/components/FIlterPost/FIlterPost.jsx
@@ -8,18 +8,37 @@ export class FIlterPost extends Component {
         filters:[],
     }
 
+
+    toggleSelection = (filter)=>{
+        if (this.state.filters.includes(filter)) {
+            this.setState({
+                filters: this.state.filters.filter((tag)=>{
+                    if (tag!==filter) return tag;
+                })
+            },()=>{this.props.filterPosts(this.state.filters)})
+        }
+
+        else{
+            let filtersTemp = [...this.state.filters]
+            filtersTemp.push(filter);
+            this.setState({
+                filters: filtersTemp,
+            },()=>{this.props.filterPosts(this.state.filters)})
+        }
+    }
+
     render() {
         let filtersTemp=[];
         this.props.posts.forEach((post)=>{
             if (filtersTemp.includes(post.type)===false) filtersTemp.push(post.type);
         })
-
+        
             return (
                 <div className="filterPostWrapper">
                     <h5>Filter by type</h5>
                     <div className="filerPostTag">
                     {filtersTemp.map((filter)=>{
-                        return <FilterTag key={filter} filter={filter}/>
+                        return <FilterTag key={filter} filter={filter} isActive={this.state.filters.includes(filter)? "active": ""} toggleSelection={this.toggleSelection}/>
                     })}
                     </div>
                 </div> 
